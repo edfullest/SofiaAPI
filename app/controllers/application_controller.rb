c367db80db647dfe11a315a0562371055bdd42fb
Eduardo Serna <lalotahoma@icloud.com>
2017-11-02 11:42:56 -0600

    Added name to the whitelist when registering

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :type, :age])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :type, :age, :name, :image])
   end
 end
